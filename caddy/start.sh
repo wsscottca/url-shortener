10b58595eb4fc4fc62c7e75987eeea9f50f956e7
Will Scott <wsscottca@gmail.com>
2024-05-09 13:02:02 -0700

    Configuring caddy

--- caddy/start.sh
+++ caddy/start.sh
@@ -6,7 +6,7 @@ if [ -z "$DOMAIN" ]
 then
     # If DOMAIN is blank, set to localhost
     # Note: in prod, domain will be the actual domain
-    export DOMAIN="ec2-54-183-133-161.us-west-1.compute.amazonaws.com"
+    export DOMAIN=""
 fi
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile

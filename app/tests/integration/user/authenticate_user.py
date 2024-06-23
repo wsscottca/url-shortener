4730acfd81d829108fadd44f82639082e0841b74
Will Scott <wsscottca@gmail.com>
2024-06-23 13:30:21 -0700

    Changing handling of invalid password - updating tests

--- app/tests/integration/user/authenticate_user.py
+++ app/tests/integration/user/authenticate_user.py
@@ -1,5 +1,7 @@
 ''' Integration tests for authenticate user '''
+import pytest
 from app.api.auth.hash_password import hash_password
+from app.api.exceptions import CredentialError
 from app.api.user.authenticate_user import authenticate_user
 from app.db.models.user import User
 
@@ -13,5 +15,6 @@ def test_authenticate_user():
 
 def test_authenticate_user_invalid():
     '''Test that if an authentication fails we return False'''
-    result = authenticate_user("test1", hash_password("test2"))
-    assert result is False
+    # Ensure our function raises the proper error
+    with pytest.raises(CredentialError):
+        authenticate_user("test1", hash_password("test2"))

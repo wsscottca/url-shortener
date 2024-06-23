2cca659893b7ca87c51156b4a37b4446ca08704c
Will Scott <wsscottca@gmail.com>
2024-06-23 13:09:37 -0700

    Changing handling of invalid password

--- app/api/user/authenticate_user.py
+++ app/api/user/authenticate_user.py
@@ -1,11 +1,12 @@
 ''' Module contains function to authenticate the user '''
 
 from app.api.auth.verify_password import verify_password
+from app.api.exceptions import CredentialError
 from app.db.services.get_user import get_user
 from app.db.models.user import User
 
 
-def authenticate_user(username: str, password: str) -> User | bool:
+def authenticate_user(username: str, password: str) -> User:
     '''
     Validates the user's password
     
@@ -13,12 +14,14 @@ def authenticate_user(username: str, password: str) -> User | bool:
         username (str): username of user to authenticate
         password (str): password of user to authenticate
 
+    Raises:
+        CredentialError: Invalid password
+        
     Returns:
         User: The authenticated
-        bool: False if the user cannot be authenticated
     '''
     user = get_user(username)
     if not verify_password(password, user.password):
-        return False
+        raise CredentialError(401, "Invalid Password.")
 
     return user
